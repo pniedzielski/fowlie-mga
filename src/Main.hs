ad7feae92f0dba03e695c5b7a4dd144276470665
Patrick M. Niedzielski <pmn25@cornell.edu>
2015-11-28 04:11:28 +0000

    Add basic MG features
    MG features are either _selectional_, in which case they drive Merge, or
    _licensing_, in which case they drive Move.  Negative selectional
    features are bare, positive selectional features have a `=` prefix,
    negative licensing features have a `-` prefix, and positive licensing
    features have a `+` prefix.
    
    Lexical items are then just pairs of tokens and feature lists.
    
    License:       GPLv3+
    Signed-off-by: Patrick M. Niedzielski <pmn25@cornell.edu>

--- src/Main.hs
+++ src/Main.hs
@@ -71,6 +71,64 @@ tokenize = words ∘ fmap toLower  -- normalize to lowercase and then
 
 
 --------------------------------------------------------------------------------
+--                                                                    GRAMMAR --
+--------------------------------------------------------------------------------
+
+
+-- | A 'Selectional' feature is one that drives Merge.  A categorial
+-- feature 𝐹 and a selector feature =𝐹 can Merge to form a larger
+-- tree.
+type Selectional = String
+
+-- | A 'Licensing' feature is one that drives Move.  A licensor
+-- feature +𝐹 and a licensee feature -𝐹 are checked by a Move to
+-- reorder the constituents in the tree.
+type Licensing   = String
+
+-- | A 'Feature' is either a selectional feature or a licensing
+-- feature.
+data Feature = Category Selectional
+             | Selector Selectional
+             | Licensor Licensing
+             | Licensee Licensing
+             deriving( Eq )
+
+instance Show Feature where
+  show (Category f) = f
+  show (Selector f) = "=" ⧺ f
+  show (Licensor f) = "+" ⧺ f
+  show (Licensee f) = "-" ⧺ f
+
+-- | 'canMergeWith' returns whether an atom with a given list of
+-- features can merge with an atom with another list of features.
+-- This is only true when the head of the lefthand features list is a
+-- selector for the head of the righthand features list.
+canMergeWith ∷ [Feature] → [Feature] → Bool
+canMergeWith ((Selector f):_) ((Category f'):_) = f ≡ f'
+canMergeWith _ _                                = False
+
+-- | A 'LexicalItem' is a pairing of an orthographic representation
+-- that appears in the surface string and a list of features that must
+-- be checked in a valid parse.
+data LexicalItem = LexicalItem (Token, [Feature])
+                 deriving( Eq )
+
+instance Show LexicalItem where
+  show (LexicalItem (token, features)) = token ⧺ " ∷ " ⧺ featureString
+    where featureString = unwords $ fmap show features
+
+-- | 'featuresOf' returns the list of features associated with a
+-- lexical item.
+featuresOf ∷ LexicalItem → [Feature]
+featuresOf (LexicalItem (_, features)) = features
+
+-- | 'tokenOf' returns the surface form associated with a lexical
+-- item.
+tokenOf ∷ LexicalItem → Token
+tokenOf (LexicalItem (token, _)) = token
+
+
+--------------------------------------------------------------------------------
 --                                                                    PARSING --
 --------------------------------------------------------------------------------
 
